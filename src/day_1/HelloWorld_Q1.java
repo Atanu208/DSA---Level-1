2a7cbff3c7e8e50e813c1922d982f1d87babcff1
Atanu <mondalatanu208@gmail.com>
2024-11-20 21:39:13 +0530

    11/20/2024 9:37PM

---
+++ src/day_1/HelloWorld_Q1.java
@@ -0,0 +1,9 @@
+package day_1;
+
+public class HelloWorld_Q1 {
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+//       How to print hello world 5 time
+        System.out.println("Hello World\nHello World\nHello World\nHello World\nHello World");
+    }
+}
